824ff67f9c16a795e3170cfb45bf58a62e083d2e
java9pro <java9pro@gmail.com>
2018-07-29 15:04:42 +0530

    committed

--- src/main/java/com/becomeJavaPro/springmvc/Utils.java
+++ src/main/java/com/becomeJavaPro/springmvc/Utils.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Utils {
     public static void main(String[] args) {
-            String password = "java9proUpdated";
+            String password = "java9proUpdate";
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
          System.out.println(passwordEncoder.encode(password));
 
